42f03f8be1a60a34c091f5d4886c0c6ef6fe7b9d
sung34 <imspins1234@gmail.com>
2023-05-29 00:01:49 +0900

    feat: fetch trending and top rated from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
-import Image from 'next/image'
+import Result from "@/components/Result";
 
-export default function Home() {
-  return (
-  <h1 className='text-red-400'>HOME</h1>
-  )
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 60 * 60 * 24} }
+  );
+  if (!response.ok) {
+    throw new Error("failed to fetch data")
+  }
+ 
+  const results = await response?.json().then(res => res.results)
+
+  return <div>
+    <Result results={results} />
+  </div>
 }
