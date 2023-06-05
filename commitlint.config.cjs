6707b5d5b07e2e8804aa5897cbd92ff53da25c1e
sung34 <imspins1234@gmail.com>
2023-06-05 22:22:39 +0900

    :sparkles: feat: 제목
    내용
    내요ㅕㅇ
    내용

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,24 +1,21 @@
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
+
 module.exports = {
   rules: {
     // @see: https://commitlint.js.org/#/reference-rules
   },
   prompt: {
     alias: { fd: "docs: fix typos" },
+    
     messages: {
       type: "깃모지 선택~",
-      customScope: "담당하신 브랜치 이름 또는 소속된 feature이름 적어주세요. 예) dashboardChart1",
       subject: "커밋 제목을 적어주세요. \n",
       body: '상세 내용을 적어주세요. 줄 바꿈은 "|" 을 사용해주세요.\n',
-      breaking: 'List any BREAKING CHANGES (optional). Use "|" to break new line:\n',
-      footerPrefixesSelect: "Select the ISSUES type of changeList by this change (optional):",
-      customFooterPrefix: "Input ISSUES prefix:",
-      footer: "List any ISSUES by this change. E.g.: #31, #34:\n",
-      confirmCommit: "이대로 커밋 하시겠습니까? (Y/N)"
+      confirmCommit: "이대로 커밋 하시겠습니까? (Y/N)",
     },
     types: [
-      { value: "✨",     name: "✨ feat:      기능", emoji: ":sparkles:" },
+      { value: "feat",     name: "✨ feat:      기능", emoji: ":sparkles:" },
       { value: "conf",     name: "⚙️  conf:      환경설정", emoji: ":gear:" },
       { value: "init",     name: "🎉 init:      설치", emoji: ":tada:" },
       { value: "fix",      name: "🔨 fix:       버그 수정", emoji: ":hammer:" },
@@ -34,36 +31,22 @@ module.exports = {
       { value: "chore",    name: "📦 chore:     패키지 설치", emoji: ":package:" },
     ],
     useEmoji: true,
-    emojiAlign: "center",
+    emojiAlign: "left",
     useAI: false,
     aiNumber: 1,
     themeColorCode: "",
     scopes: [],
-    allowCustomScopes: true,
-    allowEmptyScopes: false,
-    customScopesAlign: "bottom",
-    customScopesAlias: "custom",
-    emptyScopesAlias: "empty",
+    allowCustomScopes: false,
+    allowEmptyScopes: true,
     upperCaseSubject: false,
     markBreakingChangeMode: false,
-    allowBreakingChanges: ['feat', 'fix'],
     breaklineNumber: 80,
     breaklineChar: "|",
-    skipQuestions: [],
-    issuePrefixes: [{ value: "closed", name: "closed:   ISSUES has been processed" }],
-    customIssuePrefixAlign: "top",
-    emptyIssuePrefixAlias: "skip",
-    customIssuePrefixAlias: "custom",
-    allowCustomIssuePrefix: true,
-    allowEmptyIssuePrefix: true,
-    confirmColorize: true,
     maxHeaderLength: Infinity,
     maxSubjectLength: Infinity,
     minSubjectLength: 0,
     scopeOverrides: undefined,
     defaultBody: "",
-    defaultIssues: "",
-    defaultScope: "",
     defaultSubject: "",
     customScope: false,
   }
